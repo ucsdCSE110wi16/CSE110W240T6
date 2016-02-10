b8832e679602efc9125e6800dcc43561a4d60f93
Jenny Nguyen <ttn053@ucsd.edu>
2016-02-10 12:20:37 -0800

    FEEDS GUI

---
+++ app/src/main/java/teamjamin/ffs/CartActivity.java
@@ -0,0 +1,14 @@
+package teamjamin.ffs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jenny on 2/10/16.
+ */
+public class CartActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
