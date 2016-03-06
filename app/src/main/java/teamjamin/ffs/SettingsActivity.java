f7f521fb2dbb901e07afca55e5ff0ad8ab9aa805
ikatritc <ikatritc@ucsd.edu>
2016-03-05 17:00:15 -0800

    Main functionality has broken, may be part of android setup

--- app/src/main/java/teamjamin/ffs/SettingsActivity.java
+++ app/src/main/java/teamjamin/ffs/SettingsActivity.java
@@ -1,15 +1,20 @@
 package teamjamin.ffs;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 /**
  * Created by Jenny on 2/10/16.
  */
-public class SettingsActivity extends AppCompatActivity {protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_settings);
-}
+public class SettingsActivity extends PreferenceActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.xml.preferences);
+    }
+
 
 }
 
